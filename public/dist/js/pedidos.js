f50b73fc98a81d0d33661e4d74df6beabca72238
Micael Martinez <micaelmartinez@MacBook-Pro-de-Micael.local>
2022-08-01 08:48:49 -0300

    add scss in the page pedidos

--- public/dist/js/pedidos.js
+++ public/dist/js/pedidos.js
@@ -5,6 +5,7 @@ async function fetchText() {
         .then(res => res.json())
         .then(data => {
             let result = '';
+            let totalPrice = 0;
             data.forEach(pedido => {
                 const { username, phone, direccion, barrio, hour, minutes } = pedido
 
@@ -14,41 +15,75 @@ async function fetchText() {
                         <h2>${hour}:${minutes}</h2>
                         <p class="pedido__hour__entrega">(Horario de entrega)</p>
                     </div>
+                    <div class="pedido__title">
+                        <h3 class="pedido__title__burgers">Hamburguesas a crear</h3>
+                        <h3 class="pedido__title__details">Datos de envio</h3>
+                    </div>
                     <div class="pedido__description">
-                        <div class="pedido__burgersToDo">
+                        <div class="pedido__burgersContainer">
+                            <div class="pedido__burgersToDo">
                                 ${variety()}
+                            </div>
                         </div>
                         <div class="pedido__contact">
-                            <p>${username}</p>
-                            <p>${direccion}</p>
-                            <p>${barrio}</p>
-                            <p>${phone}</p>
+                            <p>Nombre: ${username}</p>
+                            <p>Dirección: ${direccion}</p>
+                            <p>Barrio: ${barrio}</p>
+                            <p>Teléfono: ${phone}</p>
                         </div>
                     </div>
+
+                ${priceTotal()}
                 </div>
                 `
 
                 function variety() {
                     let burgers = pedido.cart;
-                    console.log(burgers, 'q onda porq no funca')
+                    // console.log(burgers, 'q onda porq no funca')
                     let burgersHTML = '';
                     JSON.parse(burgers).forEach(variety => {
                         burgersHTML += `
-                        <div class="pedido__burgersToDo__variety">${variety.title}</div>
+                        <div class="pedido__burgersToDo__container">
+                            <div class="pedido__burgersToDo__variety">${variety.title}</div>
+                            <div class="pedido__burgersToDo__details">
+                                <p>${variety.description}</p>
+                                <p>Cantidad: ${variety.amount}</p>
+                                <p>Precio: $${variety.price}</p>
+                            </div>
+                        </div>
                         `
                     })
+
                     return burgersHTML
+                }
+
+                function priceTotal() {
+                    let burgers = pedido.cart;
+                    let totalPrice = 0;
+                    let totalPriceHTML = '';
+                    JSON.parse(burgers).forEach(variety => {
+                        totalPrice += Number(variety.amount) * Number(variety.price)
+                    })
+
+                    return `<div class="pedido__totalPrice">Precio total del pedido: $${totalPrice}</div>`
+
                 }
 
             })
 
-
-
-
-
-
             WrapperPedido.innerHTML = result;
+
+            const borderDetails = document.querySelector('.pedido__burgersToDo__details');
+            data.forEach(pedido => {
+                let burgers = pedido.cart;
+                console.log(JSON.parse(burgers).length)
+                if (JSON.parse(burgers).length <= 1) {
+                    borderDetails.style.border = 'none';
+                }
+            })
+
         })
+
 
 
 
